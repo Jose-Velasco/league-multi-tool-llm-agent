2b741ba7d6ff5b29dde8964ca1af8f2344f87984
Jose-Velasco <jose.juarez.velasco@gmail.com>
2026-05-17 03:32:07 -0700

    update

--- src/league_multi_tool_llm_agent/scripts/setup_ollama.sh
+++ src/league_multi_tool_llm_agent/scripts/setup_ollama.sh
@@ -15,7 +15,7 @@ sudo apt-get install lshw
 
 sudo apt-get install zstd
 
-sudo apt install -y pciutils
+# sudo apt install -y pciutils
 
 curl -fsSL https://ollama.com/install.sh | sh
 
@@ -26,28 +26,28 @@ nvidia-smi
 
 # Start Ollama server in background
 # ollama serve > /tmp/ollama.log 2>&1 &
-nohup ollama serve > /tmp/ollama.log 2>&1 &
+# nohup ollama serve > /tmp/ollama.log 2>&1 &
 
-# Wait until server is ready
-until curl -s http://127.0.0.1:11434/api/tags > /dev/null; do
-  echo "Waiting for Ollama..."
-  sleep 2
-done
+# # Wait until server is ready
+# until curl -s http://127.0.0.1:11434/api/tags > /dev/null; do
+#   echo "Waiting for Ollama..."
+#   sleep 2
+# done
 
-echo "Ollama is running."
+# echo "Ollama is running."
 
-# ollama serve &
-# # ollama serve --think=false
-# sleep 8
+# # ollama serve &
+# # # ollama serve --think=false
+# # sleep 8
 
-echo "Downloading Ollama qwen3.5:2b-q4_K_M..."
-ollama pull qwen3.5:2b-q4_K_M
+# echo "Downloading Ollama qwen3.5:2b-q4_K_M..."
+# ollama pull qwen3.5:2b-q4_K_M
 
-echo "Downloading gemma4:e4b-it-q4_K_M..."
-ollama pull gemma4:e4b-it-q4_K_M
+# echo "Downloading gemma4:e4b-it-q4_K_M..."
+# ollama pull gemma4:e4b-it-q4_K_M
 
-echo "Downloading qwen3-embedding:0.6b..."
-ollama pull qwen3-embedding:0.6b
+# echo "Downloading qwen3-embedding:0.6b..."
+# ollama pull qwen3-embedding:0.6b
 
-echo "Install Ollama script complete!!!!"
-ollama list
+# echo "Install Ollama script complete!!!!"
+# ollama list
